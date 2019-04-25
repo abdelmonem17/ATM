a52e685cb4f40610e1277bf02e61ea863a5a6c80
Abdelmonem Mostafa <48067992+abdelmonem17@users.noreply.github.com>
2019-04-25 12:58:52 +0200

    Update customer.c

--- APP/customer/customer.c
+++ APP/customer/customer.c
@@ -1,3 +1,14 @@
+/******************************************************************************
+ * Copyright (C) 2019 by Abdelmonem Mostafa
+ *
+ * Redistribution, modification or use of this software in source or binary
+ * forms is permitted as long as the files maintain this copyright. Users are
+ * permitted to modify this and use it to learn about the field of embedded
+ * software.
+ *
+ *****************************************************************************/
+
+
 /****************************************************************************
  * @file  customer.h
  * @brief this file includes some functions protype to manipulate customer data
