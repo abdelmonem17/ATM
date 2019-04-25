274251dff488cccc483ffdf29df1ba49f16eb0a5
Abdelmonem Mostafa <48067992+abdelmonem17@users.noreply.github.com>
2019-04-25 12:55:02 +0200

    Update customer.h

--- APP/customer/customer.h
+++ APP/customer/customer.h
@@ -1,5 +1,5 @@
 /******************************************************************************
- * Copyright (C) 2017 by Abdelmonem Mostafa
+ * Copyright (C) 2019 by Abdelmonem Mostafa
  *
  * Redistribution, modification or use of this software in source or binary
  * forms is permitted as long as the files maintain this copyright. Users are
